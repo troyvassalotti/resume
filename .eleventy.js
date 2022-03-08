096dd75fa7b7c3880dd3fcc91971be8f2be13e0c
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-03-07 21:31:46 -0500

    update resume with ccjs and image

--- .eleventy.js
+++ .eleventy.js
@@ -11,14 +11,13 @@ module.exports = function(config) {
     if (process.env.ELEVENTY_ENV === "production") {
         config.addTransform("htmlmin", function (content, outputPath) {
             if (this.outputPath && this.outputPath.endsWith(".html")) {
-                let minified = htmlmin.minify(content, {
+                return htmlmin.minify(content, {
                     useShortDoctype: true,
                     removeComments: true,
                     collapseWhitespace: true,
                     minifyCSS: true,
                     minifyJS: true,
                 });
-                return minified
             }
             return content
         })
--- src/_includes/script.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("print-this").addEventListener("click", () => window.print());
