9a18d5a085cb8652f62f06fcc4912013935b911d
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2024-01-13 16:27:04 -0500

    Update resume

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,17 @@
-const htmlmin = require("html-minifier-terser")
+module.exports = function (config) {
+  config.addPassthroughCopy({ "./public": "/" });
 
-module.exports = function(config) {
-    config.addPassthroughCopy({"./public": "/"})
+  config.addFilter("mmyyyy", function (date) {
+    let d = new Date(date);
+    return `${d.getUTCMonth() + 1}/${d.getUTCFullYear()}`;
+  });
 
-    config.addFilter("mmyyyy", function (date) {
-        let d = new Date(date)
-        return `${d.getUTCMonth() + 1}/${d.getUTCFullYear()}`
-    })
-
-    if (process.env.ELEVENTY_ENV === "production") {
-        config.addTransform("htmlmin", function (content, outputPath) {
-            if (this.outputPath && this.outputPath.endsWith(".html")) {
-                return htmlmin.minify(content, {
-                    useShortDoctype: true,
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    minifyCSS: true,
-                    minifyJS: true,
-                });
-            }
-            return content
-        })
-    }
-
-    return {
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-        dir: {
-            input: "src",
-            output: "dist"
-        }
-    }
-}
+  return {
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
