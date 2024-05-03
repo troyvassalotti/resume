6754019f8581842951171c1a493b7a6a19652e3f
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2024-05-02 23:19:08 -0400

    Create build script, output files

---
+++ src/bin/serve.rs
@@ -0,0 +1,10 @@
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(fs::Files::new("/", "./dist/").index_file("index.html")))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::fs;
+use argh::FromArgs;
+use chrono::{Datelike, NaiveDate};
+use fs_extra::{copy_items, dir};
+use minijinja::{path_loader, syntax::SyntaxConfig, AutoEscape, Environment};
+use std::fs::{self, File};
+use std::io::Write;
 use std::path::PathBuf;
 use std::result::Result;
-
-use argh::FromArgs;
-use minijinja::{syntax::SyntaxConfig, AutoEscape, Environment};
-
-use chrono::{Datelike, NaiveDate};
 
 /// A small application that renders a MiniJina template.
 #[derive(FromArgs)]
@@ -26,18 +26,11 @@ fn pretty_date(date: &str) -> String {
     format!("{month:02}/{year}")
 }
 
-// todo: glob add_templates for anything in includes
-fn execute() -> Result<(), Box<dyn std::error::Error>> {
-    let cli: Cli = argh::from_env();
-
+fn create_env() -> Environment<'static> {
     let mut env = Environment::new();
+
     env.set_auto_escape_callback(|_name| AutoEscape::None);
-    env.add_template("style.css", include_str!("_includes/style.css"))
-        .unwrap();
-    env.add_template("sprites.html", include_str!("_includes/sprites.html"))
-        .unwrap();
-    env.add_template("macros.html", include_str!("_includes/macros.html"))
-        .unwrap();
+    env.set_loader(path_loader("src/_includes"));
     env.add_filter("pretty_date", pretty_date);
     env.set_syntax(
         SyntaxConfig::builder()
@@ -46,18 +39,32 @@ fn execute() -> Result<(), Box<dyn std::error::Error>> {
             .build()
             .unwrap(),
     );
+
+    env
+}
+
+fn build() -> Result<(), Box<dyn std::error::Error>> {
+    let cli: Cli = argh::from_env();
     let source = fs::read_to_string(&cli.template)?;
     let name = cli.template.file_name().unwrap().to_str().unwrap();
+
+    let mut env = create_env();
     env.add_template(name, &source)?;
 
     let ctx: serde_json::Value = serde_json::from_slice(&fs::read(&cli.context)?)?;
-
     let tmpl = env.get_template(name).unwrap();
-    println!("{}", tmpl.render(ctx)?);
+
+    fs::create_dir_all("dist")?;
+    let mut file = File::create("dist/index.html")?;
+    file.write_all(tmpl.render(ctx)?.as_bytes())?;
+
+    let options = dir::CopyOptions::new();
+    let from_paths = vec!["public/static", "public/favicon.ico"];
+    copy_items(&from_paths, "dist", &options)?;
 
     Ok(())
 }
 
 fn main() {
-    execute().unwrap();
+    build().unwrap();
 }
